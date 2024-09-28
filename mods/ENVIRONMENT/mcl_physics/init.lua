testersakage/mineclonia-jtest

Apply entity physics to experience orbs, add node friction and make ice more slipery than other nodes, pass entity thru to effects

mods/ENVIRONMENT/mcl_physics/init.lua
@@ -8,7 +8,7 @@ dofile(modpath.."/api.lua")
 -- TODO: move to Flowlib
 local FLOW_SPEED = 1.39
 local BOUANCY = 3
-mcl_physics.register_environment_effect(function(pos, vel, staticdata)
+mcl_physics.register_environment_effect(function(pos, vel, staticdata, entity)
 	-- Get the node and node definition
 	local node = minetest.get_node_or_nil(pos); if not node then return end
 	local nodedef = minetest.registered_nodes[node.name]; if not nodedef then return end
@@ -20,17 +20,20 @@ mcl_physics.register_environment_effect(function(pos, vel, staticdata)
 
 	-- Get liquid flow direction
 	local vec = vector.multiply(flowlib.quick_flow(pos, node), FLOW_SPEED)
-	return vector.new(vec.x, -0.22, vec.z),nil
+	return vector.new(vec.x, -0.22, vec.z),nil -- TODO: move bouancy velocity out of here
 end)
 
 -- Simple gravity and bouancy
-mcl_physics.register_environment_effect(function(pos, vel, staticdata)
+mcl_physics.register_environment_effect(function(pos, vel, staticdata, entity)
 	-- Get the node and node definition
 	local node = minetest.get_node_or_nil(pos);
 	local nodedef = nil
 	if node then nodedef = minetest.registered_nodes[node.name] end
 
-	if nodedef and nodedef.liquidtype == "source" then
+	if nodedef and nodedef.liquidtype == "source" then -- TODO: make this apply to flowing liquids as well
+		-- TODO: make this not apply to fish
+		--print("entity="..dump(entity))
+
 		-- Apply decceleration and bouancy if the entity moved from flowing water to
 		-- stationary water
 		return nil,vector.new(
@@ -45,16 +48,36 @@ mcl_physics.register_environment_effect(function(pos, vel, staticdata)
 end)
 
 -- Node effects
-mcl_physics.register_environment_effect(function(pos, vel, staticdata)
-	local pos_r = vector.round(pos)
-	local node = minetest.get_node(pos_r)
-	local nodedef = minetest.registered_nodes[node.name]
-	if not nodedef then return end
+local DEFAULT_NODE_PHYSICS = {
+	friction = 0.9
+}
+local function apply_node_physics(node, vel, staticdata, entity)
+	local node_def = minetest.registered_nodes[node.name] or {}
+	local node_physics = node_def._mcl_physics or DEFAULT_NODE_PHYSICS
+	local pos = entity.object:get_pos()
 
-	if nodedef._mcl_physics_effect then
-		return nodedef._mcl_physics_effect(pos, vel, staticdata)
+	local node_physics_effect = node_physics.effect
+	if node_physics_effect then
+		return node_physics_effect(pos, vel, staticdata)
 	end
 
-	return -- nil,nil
+	-- Default behavior
+	local accel = vector.zero()
+
+	-- Friction
+	local friction_scale = node_physics.friction
+	accel = accel + vel * -friction_scale
+
+	return vector.zero(), accel
+end
+mcl_physics.register_environment_effect(function(pos, vel, staticdata, entity)
+	local pos1_r = vector.round(pos)
+	local v1,a1 = apply_node_physics(minetest.get_node(pos1_r), vel, staticdata, entity)
+
+	-- TODO: only apply when touching under_node
+	local pos2_r = vector.offset(pos1_r,0,-1,0)
+	local v2,a2 = apply_node_physics(minetest.get_node(pos2_r), vel, staticdata, entity)
+
+	return (v1 + v2), (a1 + a2)
 end)
 

KenYam-Phoenixx/VoxeLibre

mods/ENVIRONMENT/mcl_physics/init.lua
@@ -9,7 +9,7 @@ dofile(modpath.."/api.lua")
 -- TODO: move to Flowlib
 local FLOW_SPEED = 1.39
 local BOUANCY = 3
-mod.register_environment_effect(function(pos, vel, staticdata)
+mod.register_environment_effect(function(pos, vel, staticdata, entity)
 	-- Get the node and node definition
 	local node = minetest.get_node_or_nil(pos); if not node then return end
 	local nodedef = minetest.registered_nodes[node.name]; if not nodedef then return end
@@ -21,17 +21,20 @@ mod.register_environment_effect(function(pos, vel, staticdata)
 
 	-- Get liquid flow direction
 	local vec = vector.multiply(flowlib.quick_flow(pos, node), FLOW_SPEED)
-	return vector.new(vec.x, -0.22, vec.z),nil
+	return vector.new(vec.x, -0.22, vec.z),nil -- TODO: move bouancy velocity out of here
 end)
 
 -- Simple gravity and bouancy
-mod.register_environment_effect(function(pos, vel, staticdata)
+mod.register_environment_effect(function(pos, vel, staticdata, entity)
 	-- Get the node and node definition
 	local node = minetest.get_node_or_nil(pos);
 	local nodedef = nil
 	if node then nodedef = minetest.registered_nodes[node.name] end
 
-	if nodedef and nodedef.liquidtype == "source" then
+	if nodedef and nodedef.liquidtype == "source" then -- TODO: make this apply to flowing liquids as well
+		-- TODO: make this not apply to fish
+		--print("entity="..dump(entity))
+
 		-- Apply decceleration and bouancy if the entity moved from flowing water to
 		-- stationary water
 		return nil,vector.new(
@@ -46,16 +49,35 @@ mod.register_environment_effect(function(pos, vel, staticdata)
 end)
 
 -- Node effects
-mod.register_environment_effect(function(pos, vel, staticdata)
-	local pos_r = vector.round(pos)
-	local node = minetest.get_node(pos_r)
-	local nodedef = minetest.registered_nodes[node.name]
-	if not nodedef then return end
+local DEFAULT_NODE_PHYSICS = {
+	friction = 0.9
+}
+local function apply_node_physics(node, vel, staticdata, entity)
+	local node_def = minetest.registered_nodes[node.name] or {}
+	local node_physics = node_def._mcl_physics or DEFAULT_NODE_PHYSICS
 
-	if nodedef._mcl_physics_effect then
-		return nodedef._mcl_physics_effect(pos, vel, staticdata)
+	local node_physics_effect = node_physics.effect
+	if node_physics_effect then
+		return node_physics_effect(pos, vel, staticdata)
 	end
 
-	return -- nil,nil
+	-- Default behavior
+	local accel = vector.zero()
+
+	-- Friction
+	local friction_scale = node_physics.friction
+	accel = accel + vel * -friction_scale
+
+	return vector.zero(), accel
+end
+mod.register_environment_effect(function(pos, vel, staticdata, entity)
+	local pos1_r = vector.round(pos)
+	local v1,a1 = apply_node_physics(minetest.get_node(pos1_r), vel, staticdata, entity)
+
+	-- TODO: only apply when touching under_node
+	local pos2_r = vector.offset(pos1_r,0,-1,0)
+	local v2,a2 = apply_node_physics(minetest.get_node(pos2_r), vel, staticdata, entity)
+
+	return (v1 + v2), (a1 + a2)
 end)
 